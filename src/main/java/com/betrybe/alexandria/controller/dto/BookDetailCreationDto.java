faaca0d229d973d0a4c612d8dfec5a834f8cc733
rafael <rafaelmagalhaes1@live.com>
2024-06-01 14:40:36 -0300

    feat: BookDetailCreationDto created

---
+++ src/main/java/com/betrybe/alexandria/controller/dto/BookDetailCreationDto.java
@@ -0,0 +1,18 @@
+package com.betrybe.alexandria.controller.dto;
+
+import com.betrybe.alexandria.entity.BookDetail;
+
+/**
+ * The type Book detail creation dto.
+ */
+public record BookDetailCreationDto(String summary, Integer pageCount, Integer year, String isbn) {
+
+  /**
+   * To entity book detail.
+   *
+   * @return the book detail
+   */
+  public BookDetail toEntity() {
+    return new BookDetail(summary, pageCount, year, isbn);
+  }
+}
