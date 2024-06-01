a624f49afc922b6be82197a5ff9c4fb5a28cabf7
rafael <rafaelmagalhaes1@live.com>
2024-06-01 14:35:25 -0300

    feat: Author Repository created

---
+++ src/main/java/com/betrybe/alexandria/repository/AuthorRepository.java
@@ -0,0 +1,13 @@
+package com.betrybe.alexandria.repository;
+
+import com.betrybe.alexandria.entity.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * The interface Author repository.
+ */
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+}
