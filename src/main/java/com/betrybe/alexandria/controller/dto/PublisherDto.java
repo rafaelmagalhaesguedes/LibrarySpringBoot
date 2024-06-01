24025def5b564ea289a742c4688ec1d58b123bd2
rafael <rafaelmagalhaes1@live.com>
2024-06-01 14:39:55 -0300

    feat: PublisherDto created

---
+++ src/main/java/com/betrybe/alexandria/controller/dto/PublisherDto.java
@@ -0,0 +1,23 @@
+package com.betrybe.alexandria.controller.dto;
+
+import com.betrybe.alexandria.entity.Publisher;
+
+/**
+ * The type Publisher dto.
+ */
+public record PublisherDto(Long Id, String name, String address) {
+
+  /**
+   * From entity publisher dto.
+   *
+   * @param publisher the publisher
+   * @return the publisher dto
+   */
+  public static PublisherDto fromEntity(Publisher publisher) {
+    return new PublisherDto(
+        publisher.getId(),
+        publisher.getName(),
+        publisher.getAddress()
+    );
+  }
+}
