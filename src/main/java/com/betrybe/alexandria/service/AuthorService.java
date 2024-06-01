4eef0bd3bb30968ba5a7c32a474627f8dadc8adf
rafael <rafaelmagalhaes1@live.com>
2024-06-01 14:39:01 -0300

    feat: Author Service created

---
+++ src/main/java/com/betrybe/alexandria/service/AuthorService.java
@@ -0,0 +1,92 @@
+package com.betrybe.alexandria.service;
+
+import com.betrybe.alexandria.entity.Author;
+import com.betrybe.alexandria.repository.AuthorRepository;
+import com.betrybe.alexandria.service.exception.AuthorNotFoundException;
+import com.betrybe.alexandria.service.exception.BookNotFoundException;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * The type Author service.
+ */
+@Service
+public class AuthorService {
+
+  private final AuthorRepository authorRepository;
+
+  /**
+   * Instantiates a new Author service.
+   *
+   * @param authorRepository the author repository
+   */
+  @Autowired
+  public AuthorService(AuthorRepository authorRepository) {
+    this.authorRepository = authorRepository;
+  }
+
+  /**
+   * Find by id author.
+   *
+   * @param id the id
+   * @return the author
+   * @throws AuthorNotFoundException the author not found exception
+   */
+  public Author findById(Long id) throws AuthorNotFoundException {
+    return authorRepository.findById(id)
+        .orElseThrow(AuthorNotFoundException::new);
+  }
+
+  /**
+   * Find all list.
+   *
+   * @return the list
+   */
+  public List<Author> findAll() {
+    return authorRepository.findAll();
+  }
+
+  /**
+   * Create author.
+   *
+   * @param author the author
+   * @return the author
+   */
+  public Author create(Author author) {
+    return authorRepository.save(author);
+  }
+
+  /**
+   * Update author.
+   *
+   * @param id     the id
+   * @param author the author
+   * @return the author
+   * @throws AuthorNotFoundException the author not found exception
+   */
+  public Author update(Long id, Author author) throws AuthorNotFoundException {
+    Author authorFromDb = findById(id);
+
+    authorFromDb.setName(author.getName());
+    authorFromDb.setNationality(author.getNationality());
+
+    return authorRepository.save(authorFromDb);
+  }
+
+  /**
+   * Delete by id author.
+   *
+   * @param id the id
+   * @return the author
+   * @throws AuthorNotFoundException the author not found exception
+   */
+  public Author deleteById(Long id) throws AuthorNotFoundException {
+    // Pegamos a entidade antes de apagar, para poder retorná-la
+    Author author = findById(id);
+
+    authorRepository.deleteById(id);
+
+    return author;
+  }
+}
