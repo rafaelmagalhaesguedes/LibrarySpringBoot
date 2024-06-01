785cd1956ffe41b7caafa9dbd027b303ea5afc33
rafael <rafaelmagalhaes1@live.com>
2024-06-01 14:35:59 -0300

    feat: Book Detail Repository created

---
+++ src/main/java/com/betrybe/alexandria/repository/BookDetailRepository.java
@@ -0,0 +1,13 @@
+package com.betrybe.alexandria.repository;
+
+import com.betrybe.alexandria.entity.BookDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * The interface Book detail repository.
+ */
+@Repository
+public interface BookDetailRepository extends JpaRepository<BookDetail, Long> {
+
+}
